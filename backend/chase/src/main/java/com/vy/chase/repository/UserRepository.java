f0a4818a28b6e66d61c92dc59e0bfeca92d66691
lilyvx <natallielily@gmail.com>
2026-07-18 12:48:29 +0800

    delete webconfig

--- backend/chase/src/main/java/com/vy/chase/repository/UserRepository.java
+++ backend/chase/src/main/java/com/vy/chase/repository/UserRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.vy.chase.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
 }
